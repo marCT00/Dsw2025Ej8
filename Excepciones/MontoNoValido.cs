13f3a274b7f2c78e33c17924f243aa89a0cde32c
Mar <ingmarsaave@gmail.com>
2025-05-02 19:29:08 -0300

    Se agregó la automatizacion de la funcion TasaDeInteres

---
+++ Excepciones/MontoNoValido.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dsw2025Ej8.Excepciones
+{
+    public class MontoNoValido : Exception
+    {
+
+        public MontoNoValido(string? message= "El monto ingresado no es valido para la operacion solicitada") : base(message)
+        {
+        }
+    }
+}
